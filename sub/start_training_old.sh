9df8ea680f2db558963a2fb3ea874d0fe6c70d1c
Aurelio Amerio <aure.amerio@gmail.com>
2025-10-03 16:25:29 +0200

    wip

---
+++ sub/start_training_old.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /lhome/ific/a/aamerio/data/github/GenSBI-examples
+eval "$(conda shell.bash hook)"
+conda activate /lhome/ific/a/aamerio/miniforge3/envs/gensbi
+python scripts/train_sbi_model_simformer_v6.py --config $1
+
+exit
