ba8a637a51c37e47d8efb62c49aa160afe22481c
Aurelio Amerio <aure.amerio@gmail.com>
2026-02-11 18:05:45 +0100

    up

--- sub/train_all_models.sh
+++ sub/train_all_models.sh
@@ -9,3 +9,5 @@ condor_submit train_model_slcp.sub
 condor_submit train_model_bernoulli_glm.sub
 condor_submit train_model_gaussian_linear.sub
 condor_submit train_model_gaussian_mixture.sub
+condor_submit train_model_lensing.sub
+condor_submit train_model_gw.sub
